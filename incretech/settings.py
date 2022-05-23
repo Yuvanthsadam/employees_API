0b382981d472d80656ad31f55ac83f7a8aaedb5b
Yuvanth <sadamyuvanth@gmail.com>
2022-05-23 15:03:04 +0530

    initial commit

--- incretech/settings.py
+++ incretech/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used  production secret!
 SECRET_KEY = 'django-insecure-1e*30(f6$_e*w_y#^=!ck)siv7n3-^i93(5x*=dv5##wxl6(hg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
